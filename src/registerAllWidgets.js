9050617028a517b00c7e7046b04b32ba9ff72596
jhsware <sebastian@urbantalk.se>
2015-05-15 15:19:41 +0200

    Remove a bit too much...

--- src/registerAllWidgets.js
+++ src/registerAllWidgets.js
@@ -2,12 +2,14 @@
 var registry = require('protoncms-core').registry;
 var formFieldWidgets = require('./form_field_widgets');
 
-registry.registerAdapter(formFieldWidgets.TextInputAdapter);
-registry.registerAdapter(formFieldWidgets.IntegerInputAdapter);
-registry.registerAdapter(formFieldWidgets.EmailInputAdapter);
+module.exports = function () {
+    registry.registerAdapter(formFieldWidgets.TextInputAdapter);
+    registry.registerAdapter(formFieldWidgets.IntegerInputAdapter);
+    registry.registerAdapter(formFieldWidgets.EmailInputAdapter);
 
-registry.registerAdapter(formFieldWidgets.TextAreaInputAdapter);
+    registry.registerAdapter(formFieldWidgets.TextAreaInputAdapter);
 
-registry.registerAdapter(formFieldWidgets.DropDownAdapter);
+    registry.registerAdapter(formFieldWidgets.DropDownAdapter);
 
-registry.registerUtility(formFieldWidgets.ActionButtonUtility);
+    registry.registerUtility(formFieldWidgets.ActionButtonUtility);    
+}
